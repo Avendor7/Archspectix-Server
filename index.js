6438ee20f33f004a536216e57ccc7e51b1e46e84
StephenW <steve.wiggins7@gmail.com>
2024-09-15 21:50:55 -0400

    fix cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import axios from 'axios';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.get('/append', (req, res) => {
     const url = 'https://archlinux.org/packages/search/json/?q=';
     //new change
