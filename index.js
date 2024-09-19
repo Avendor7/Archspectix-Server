1d4c453d36814cc7d2cad27738bda3b275695ecc
StephenW <steve.wiggins7@gmail.com>
2024-09-18 20:22:28 -0400

    two endpoints

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.get('/append', (req, res) => {
+app.get('/aur', (req, res) => {
+    const url = 'https://aur.archlinux.org/rpc/v5/search/';
+    //new change
+    const value = req.query.value;
+
+    if (!value) {
+        return res.status(400).json({ error: 'Value parameter is required' });
+    }
+
+    axios.get(`${url}${value}`)
+        .then((response) => {
+            res.json(response.data);
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        });
+});
+
+app.get('/alr', (req, res) => {
     const url = 'https://archlinux.org/packages/search/json/?q=';
     //new change
     const value = req.query.value;
